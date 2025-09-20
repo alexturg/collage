495178a610f9fed51ddff83779cc24d8cbf69bd0
Alexandra <167291579+alexturg@users.noreply.github.com>
2025-09-20 19:04:11 +0500

    Make Tk-based tests headless-friendly

--- test/test_CollageTree.py
+++ test/test_CollageTree.py
@@ -1,11 +1,9 @@
 from src.CollageTree import CollageRoot, ResizableLeaf
 from src.Collage import Collage
 from src.CollageImage import safe_open_image
-from src.scroll import ScrolledFrame
 
 import pytest
 import os
-import tkinter as tk
 
 
 @pytest.fixture
@@ -14,9 +12,12 @@ def filename():
 
 
 @pytest.fixture
-def collage():
-    collage = Collage(0, 1, 1, 1, None, [], {'width': 30, 'height': 30})
-    return collage
+def collage(tk_root):
+    collage = Collage(0, 1, 1, 1, None, [tk_root], {'width': 30, 'height': 30})
+    try:
+        yield collage
+    finally:
+        collage.destroy()
 
 
 @pytest.fixture
--- test/test_run.py
+++ test/test_run.py
@@ -1,15 +1,14 @@
 from src.mainwindow import Application
 from src.textconfig import TextConfigureApp
-import tkinter as tk
 
 
-def test_run_Application():
-    app = Application(master=tk.Tk())
+def test_run_Application(tk_root):
+    app = Application(master=tk_root)
     app.update()
     app.destroy()
 
 
-def test_run_TextConfigureApp():
-    app = TextConfigureApp(master=tk.Tk())
+def test_run_TextConfigureApp(tk_root):
+    app = TextConfigureApp(master=tk_root)
     app.update()
     app.destroy()
